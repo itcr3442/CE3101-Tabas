89af2e99464d439ada0c7367b44fb65753926e90
Jose Retana <joseantonioretanacorrales@gmail.com>
2022-03-18 13:36:10 -0600

    Fixed users not showing up

--- web/src/app/components/user-register/user-register.component.ts
+++ web/src/app/components/user-register/user-register.component.ts
@@ -3,13 +3,16 @@ import { Router } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { RepositoryService } from 'src/app/services/repository.service';
 import { AuthService } from 'src/app/services/auth.service';
-
+import { Workers } from 'src/app/interfaces/workers.model';
+import { Users } from 'src/app/interfaces/Users.model';
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
   styleUrls: ['./user-register.component.css']
 })
 export class UserRegisterComponent implements OnInit {
+
+  public users_list!: Users[];
 
   registerForm = new FormGroup({
     id: new FormControl('', [Validators.required, Validators.pattern('[0-9]*')]),
@@ -22,9 +25,26 @@ export class UserRegisterComponent implements OnInit {
 
   message: string = ""
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private repo: RepositoryService,
+    private authService: AuthService
+  ) {
+  }
+
+  public getAllUsers = () =>{
+    let registerUrl = "usuarios"
+    console.log(registerUrl);
+    this.repo.getData(registerUrl)
+    .subscribe(res => {
+        console.log("Result:" + JSON.stringify(res));
+        this.users_list = res as Users[];
+      }
+    )
+  }
 
   ngOnInit(): void {
+    this.getAllUsers();
   }
 
   onSubmit() {
---
+++ web/src/app/interfaces/Users.model.ts
@@ -0,0 +1,8 @@
+export interface Users{
+    cedula: number;
+    password_hash: string;
+    nombre: string;
+    primer_apellido: string;
+    segundo_apellido: string;
+    telefono: number;
+}
